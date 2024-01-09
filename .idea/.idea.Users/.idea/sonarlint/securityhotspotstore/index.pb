0edadb79fcf8008eefa0aaa9cbde5dcb2481038d
Nguyen Duc Minh (K16_HCM) <minhndse161781@fpt.edu.vn>
2024-01-09 08:05:52 +0700

    first commit

---
+++ .idea/.idea.Users/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+X
+(Users.Api.Tests.Unit/UserServiceTests.cs,b\c\bc97437d0463d32de1e8ac1b7069fe8e67ba7787
+Q
+!Users.Api/Services/UserService.cs,2\8\282c8868e3c866164fb2de7d4e8d74b10f48b711
---
+++ Users.Api.Tests.Unit/UserServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Users.Api.Logging;
+using Users.Api.Models;
+using Users.Api.Repositories;
+using Users.Api.Services;
+using Xunit;
+
+namespace Users.Api.Tests.Unit
+{
+    public class UserServiceTests
+    {
+        private UserService _sut;
+        private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
+        private readonly ILoggerAdapter<UserService> _logger = Substitute.For<ILoggerAdapter<UserService>>();
+        public UserServiceTests()
+        {
+            _sut = new UserService(_userRepository, _logger);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnUsers_WhenUsersExist()
+        {
+            // Arrange
+            var users = new List<User>()    
+            {
+                new User
+                {
+                    Id = Guid.NewGuid(),
+                    FullName = "Test",
+                }
+            };
+            _userRepository.GetAllAsync().Returns(users);
+
+            // Act
+            var result = await _sut.GetAllAsync();
+
+            // Assert
+            Assert.Equal(result, users);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShouldReturnNull_WhenThatUserDoesNotExist()
+        {
+            // Arrange
+            _userRepository.GetByIdAsync(Arg.Any<Guid>()).ReturnsNull();
+
+            // Act
+            var result = await _sut.GetByIdAsync(Guid.NewGuid());
+
+            // Assert
+            result.Should().BeNull();
+        }
+    }
+}
